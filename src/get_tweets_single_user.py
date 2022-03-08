4eb0eb3da84978fde987dd71c7afcea23e3bfd55
Bais <32563996+nilanbais@users.noreply.github.com>
2022-03-08 14:16:59 +0100

    Update get_tweets_single_user.py

--- src/get_tweets_single_user.py
+++ src/get_tweets_single_user.py
@@ -5,12 +5,13 @@ extract_followers.py script.
 
 [x] - get tweets using api endpoint
 [x] - get the time of tweet with the tweet itself
-[] - be able to save or return tweets as collections (dtype: still unknkown) strings (as-is) of a single user.
+[x] - be able to return tweets as collections (dtype: list of dicts) strings (as-is) of a single user.
 [] - be able to import the script or functions in other scripts.
 
 """
 import requests
 import dotenv
+import json
 
 # env variables
 __config = dotenv.dotenv_values('./res/.env')
@@ -57,11 +58,40 @@ def connect_to_endpoint(url, params):
         )
     return response.json()
 
+def transform_response(original_response: dict, user_id: str) -> dict:
+    """
+    Function to transform the original response data so it's a combination of the following datapoints.
+    - user_id
+    - tweet_id
+    - created_at
+    - text
+    """
+    _og_package = original_response.copy()
+    _og_data = _og_package['data']
+    _og_meta = _og_package['meta']
+    transformed_data = list()
+
+    for item in _og_data:
+
+        created_at = item['created_at']
+        tweet_id = item['id']
+        text = item['text']
+
+        transformed_data.append({"user_id": user_id, "tweet_id": tweet_id, "created_at": created_at, 'text': text})
+
+    return {'data': transformed_data, 'meta': _og_meta}
+
+def store_response(json_data: dict):
+    with open('./data/tweets_user_{}.json'.format(user_id), 'w') as json_file:
+        json.dump(json_data, json_file)
+
 def main():
     url = create_url()
     params = get_params()
     json_response = connect_to_endpoint(url, params)
-    print(json_response)
+    clean_response = transform_response(original_response=json_response, user_id=user_id)
+    store_response(json_data=clean_response)
+
 
 
 if __name__ == '__main__':
