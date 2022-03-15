5dcb79652e07c04de666523b48b5735f65ce47d3
Bais <32563996+nilanbais@users.noreply.github.com>
2022-03-15 16:55:42 +0100

    Update get_tweets_single_user.py

--- src/get_tweets_single_user.py
+++ src/get_tweets_single_user.py
@@ -6,7 +6,7 @@ extract_followers.py script.
 [x] - get tweets using api endpoint
 [x] - get the time of tweet with the tweet itself
 [x] - be able to return tweets as collections (dtype: list of dicts) strings (as-is) of a single user.
-[] - be able to extract all the tweet within a given/specified time range of one user and combining the 
+[x] - be able to extract all the tweet within a given/specified time range of one user and combining the 
      multiple response packages to one single package.
 [] - be able to import the script or functions in other scripts.
 
@@ -22,9 +22,11 @@ __config = dotenv.dotenv_values('./res/.env')
 __BEARER_TOKEN = __config["BEARER_TOKEN"]
 __USERNAME = __config["USERNAME"]
 
+__TWITTER_API_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.000Z"
+
 # Input variables
 user_id = "969716112752553985"  # first from ct_accounts.json
-most_recent_time = '2021-10-22T10:30:01Z'  # HAS TO BE format like YYYY-MM-DDTHH:mm:ssZ
+most_recent_time = '2021-10-22T10:30:01.000Z'  # HAS TO BE format like YYYY-MM-DDTHH:mm:ss.000Z
 
 # Functions
 def bearer_oauth(r):
@@ -37,10 +39,10 @@ def bearer_oauth(r):
     return r
 
 def create_url(user_id: str):
-    url = "https://api.twitter.com/2/users/{}/{}?{}={}".format(user_id, 'tweets', 'max_results', 100)
+    url = "https://api.twitter.com/2/users/{}/{}?{}={}".format(user_id, 'tweets', 'max_results', 10)
     return url
 
-def get_params() -> dict:
+def get_params(end_time: str) -> dict:
     # Tweet fields are adjustable.
     # Options include:
     # attachments, author_id, context_annotations,
@@ -48,7 +50,7 @@ def get_params() -> dict:
     # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
     # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
     # source, text, and withheld
-    return {'end_time': most_recent_time, 'tweet.fields': 'created_at'}
+    return {'end_time': end_time, 'tweet.fields': 'created_at'}
 
 def connect_to_endpoint(url, params):
     response = requests.request("GET", url, auth=bearer_oauth, params=params)
@@ -93,15 +95,16 @@ def store_response(json_data: dict):
     with open('./data/tweets_user_{}.json'.format(user_id), 'w') as json_file:
         json.dump(json_data, json_file)
 
-def get_timedelta(time_range: str = 'day') -> timedelta:
+def get_stop_search_time(start_time: Union[str, datetime], time_range: str = 'day') -> timedelta:
     """
-    Based on the input this functions returns a timedelta object of the specified time range. default value = 1 day
+    Based on the input this functions returns a stop time to use as input in the recursive search for tweets
     """
-    x = 1.0
+    start_time = datetime.strptime(start_time, __TWITTER_API_TIME_FORMAT) if isinstance(start_time, str) else start_time
+
     if time_range == 'day':
-        return timedelta(days=x)
+        return start_time - timedelta(days=1)
     elif time_range == 'hour':
-        return timedelta(hours=x)
+        return start_time - timedelta(hours=1)
     else:
         raise Exception('Please specify one of the following time_ranges: \'day\', \'hour\'.')
 
@@ -109,25 +112,28 @@ def isolate_time_oldest_object(oldest_id: str, response_data: list) -> Union[dat
     """
     Isolates the object with the given id from the given response data.
     """
-    for item in response_data[:-1]:
+    for item in response_data[::-1]:
         if item['id'] == oldest_id:
-            return datetime.strptime(item['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+            return datetime.strptime(item['created_at'], __TWITTER_API_TIME_FORMAT)
         else:
             pass
     return False
 
-def get_tweets(user_id: str, time_range: str = 'day'):
+def get_tweets(user_id: str, start_search_time: Union[str, datetime] = None, stop_search_time: Union[str, datetime] = None):
     """
     The function extracts the tweets of a given/specified user, within the given time range.
     """
+    print('running get_tweets()')
     # Get the oldest time in a datetime object
-    dt_timedelta = get_timedelta(time_range=time_range)
-    dt_most_recent = datetime.strptime(most_recent_time, "%Y-%m-%dT%H:%M:%SZ")
-    dt_stop_time = dt_most_recent - dt_timedelta
+    dt_most_recent = datetime.strptime(start_search_time, __TWITTER_API_TIME_FORMAT) if isinstance(start_search_time, str) else start_search_time
+    dt_most_recent_string = get_RFC_timestamp(dt_object=dt_most_recent)
+
+    dt_stop_time = datetime.strptime(stop_search_time, __TWITTER_API_TIME_FORMAT) if isinstance(stop_search_time, str) else stop_search_time
+    dt_stop_time_string = get_RFC_timestamp(dt_object=dt_stop_time)
 
     # Prepare the api request
     url = create_url(user_id=user_id)
-    params = get_params()
+    params = get_params(end_time=dt_most_recent_string)
 
     # Make the api request
     json_response = connect_to_endpoint(url, params)
@@ -137,25 +143,38 @@ def get_tweets(user_id: str, time_range: str = 'day'):
 
     # Extract the oldest time seen in the api response
     oldest_time_response_data = isolate_time_oldest_object(oldest_id=oldest_id, response_data=json_response['data'])
+    # print(oldest_time_response_data, type(oldest_time_response_data))
 
     # Checking if the oldest seen time in response is older then the time we actually need (outside of specified time range)
-    seen_time_within_range = False if (dt_stop_time - oldest_time_response_data <= timedelta(0)) else True
+    oldest_time_within_range = False if (dt_stop_time - oldest_time_response_data <= timedelta(0)) else True
+    
+    # Filtering the json_response to only contain the times within the time range
+    return_data = [item for item in json_response['data'] if (datetime.strptime(item['created_at'], __TWITTER_API_TIME_FORMAT) - dt_stop_time) >= timedelta(0)]
+    # print(return_data, len(return_data))
+    
+    # break case of the recursive function
+    if oldest_time_within_range:
+        return return_data
+    
+    return return_data + get_tweets(user_id=user_id, start_search_time=oldest_time_response_data, stop_search_time=stop_search_time)
 
+def get_RFC_timestamp(dt_object: datetime):
     """
-    Pick it up here.
-
-    You've got the check if the oldest seen time is within the specified timerange or outside of it.
-    Based on the result of that check, you have to specify what needs to be done after
-        when within time_range -> do another request
-        when outside time_range -> drop all the older objects and store the correct objects.
-
-    Idea: explore the possibility of using recursion
+    returns a string that is a valid RFC3339 date and time notation
     """
-    pass
+    __stamp_format = "%Y-%m-%dT%H:%M:%S.000Z"
+    return datetime.strftime(dt_object, __stamp_format)
+    
 
 def main():
-    x = get_tweets(user_id=user_id)
-    print(x)
+    stop_search_time = get_stop_search_time(start_time=most_recent_time)
+    print(f'start time : {most_recent_time}, stop time : {stop_search_time}')
+    x = get_tweets(
+        user_id=user_id, 
+        start_search_time=most_recent_time, 
+        stop_search_time=stop_search_time)
+
+    print(x, len(x))
     
     # url = create_url(user_id=user_id)
     # params = get_params()
@@ -163,8 +182,6 @@ def main():
     # print(json_response)
     # clean_response = transform_response(original_response=json_response, user_id=user_id)
     # store_response(json_data=clean_response)
-
-
 
 if __name__ == '__main__':
     main()
