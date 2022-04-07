e7bc830b42a126b3d6de4bcc6fc289f2ef75ed1e
Bais <32563996+nilanbais@users.noreply.github.com>
2022-04-07 20:26:25 +0200

    Create twitter_handling.py

---
+++ src/classes/twitter_handling.py
@@ -0,0 +1,21 @@
+"""
+This script is responsible for both the interaction with the Twitter API and all the trasformations that
+need to happen on this data, recieved as a response from the API.
+"""
+import dotenv
+
+from api_authentication_class import ApiAuthentication
+
+
+class TwitterAPI(ApiAuthentication):
+    def __init__(self) -> None:
+        super().__init__('TWITTER_BEARER_TOKEN')
+        self.__config = dotenv.dotenv_values('./res/.env')
+    
+    def create_header(self) -> dict: 
+        self.header = {"User-Agent": "v2FollowingLookupPython"}
+
+    def create_url(self) -> str:
+        __USER_ID = self.__config["USER_ID"]
+        self.url = "https://api.twitter.com/2/users/{}/following?{}={}".format(__USER_ID, "max_results", 1000)
+        return self.url
