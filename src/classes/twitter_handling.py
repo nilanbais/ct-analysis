d40f4a67b59b6f6c316860fd98cc313b3bcdf076
Bais <32563996+nilanbais@users.noreply.github.com>
2022-04-14 14:08:56 +0200

    Update twitter_handling.py

--- src/classes/twitter_handling.py
+++ src/classes/twitter_handling.py
@@ -36,6 +36,9 @@ class TwitterAPI(ApiAuthentication):
 
         de methofd is belangrijk in de recursieve method get_tweets (komt nog). Bouw deze 
         gelijk goed in. 
+
+        user_id moet altijd de eerst parameter zijn, omdat deze zijn eigen plaats heeft in de query path
+        De overige query parameters kunnnen toegevoegd worden volgens de uitbeidingen :parameter1=:waarde&:parameter2=:waarde&etc
         """
         self.parameters = parameter_dict.copy()
     
