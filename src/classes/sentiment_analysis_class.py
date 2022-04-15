99c37fa9a9377e7b4308cfb372d1428736136da0
Bais <32563996+nilanbais@users.noreply.github.com>
2022-04-15 16:46:52 +0200

    Update sentiment_analysis_class.py

--- src/classes/sentiment_analysis_class.py
+++ src/classes/sentiment_analysis_class.py
@@ -5,12 +5,13 @@ and returning a sentiment score.
 import textblob
 
 
+
 class SentimentAnalysis:
 
     def __init__(self) -> None:
         pass
     
-    def get_text_sentiment(input_text: str):
+    def get_text_sentiment(self, input_text: str):
         """
         Function returns a sentiment object with the following scores.
         - polarity score -> how possitive or negative a text is.
@@ -21,3 +22,12 @@ class SentimentAnalysis:
         # Assigning the sentiment attribute to it's own variable + returning variable
         senitment_object = text_object.sentiment
         return senitment_object
+
+def main():
+    text = 'sounds good mate'
+    sa = SentimentAnalysis()
+    sentiment = sa.get_text_sentiment(text)
+    print(sentiment.polarity)
+
+if __name__ == "__main__":
+    main()
