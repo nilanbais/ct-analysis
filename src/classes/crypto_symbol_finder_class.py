d046db90dba5f38886d414527e6e083f6c70fe29
Bais <32563996+nilanbais@users.noreply.github.com>
2022-04-15 17:20:04 +0200

    Create crypto_symbol_finder_class.py

---
+++ src/classes/crypto_symbol_finder_class.py
@@ -0,0 +1,13 @@
+"""
+This script contains a class that is used in finding crypto symbols in text. 
+
+With that comes the possibility to extend this class to a wider text analysis 
+applicaitons. when that happens, think about making SentimentAnalysis a parent
+class.
+
+"""
+
+class CryptoSymbolFinder:
+
+    def __init__(self) -> None:
+        pass
