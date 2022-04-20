1c3a1f7d373c19649ca2fe64df665b64fd29b0b5
Bais <32563996+nilanbais@users.noreply.github.com>
2022-04-20 10:15:13 +0200

    Update crypto_symbol_finder_class.py

--- src/classes/crypto_symbol_finder_class.py
+++ src/classes/crypto_symbol_finder_class.py
@@ -5,9 +5,21 @@ With that comes the possibility to extend this class to a wider text analysis
 applicaitons. when that happens, think about making SentimentAnalysis a parent
 class.
 
+A crypto symbol is a short name mostly consisting of three cap letters, 
+like BTC or ETH. Twitter has a functionality where you can search the 
+platform for tweets containing crypto symbols using a '$' in front of the 
+symbols.
+
 """
 
 class CryptoSymbolFinder:
 
     def __init__(self) -> None:
         pass
+
+    def find_crypto_symbol(self, input_text: str) -> str:
+        """
+        Method used to find a crypto symbol and returns the symbols found
+        """
+        pass
+
