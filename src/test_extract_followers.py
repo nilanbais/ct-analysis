e071e5defe33251c73e7f9372ed6f9fbbf2724db
Bais <32563996+nilanbais@users.noreply.github.com>
2022-04-29 17:18:57 +0200

    Update test_extract_followers.py

--- src/test_extract_followers.py
+++ src/test_extract_followers.py
@@ -5,6 +5,24 @@ The code needs to do the following:
 - [] prep the data structure user_document.py for each user
 - [] upload the data to the database (or print if no upload method available yet) 
 """
-from cta_classes.pipeline_class import Pipeline
+import pprint
 
-upload_followed_accounts_base_user = Pipeline()
+from matplotlib.pyplot import get
+from cta_classes.pipeline_class import Pipeline
+from cta_classes.twitter_handling_classes import TwitterAPI, TwitterDataTransformer
+
+get_followed_acc_line = Pipeline()
+
+twitter_api = TwitterAPI()
+twitter_data_transformer = TwitterDataTransformer()
+
+@get_followed_acc_line.task()
+def get_list():
+    return twitter_api.extract_follers_list()
+
+@get_followed_acc_line.task(depends_on=get_list)
+def print_result(input_data):
+    pprint(input_data)
+
+
+result = get_followed_acc_line.run()
