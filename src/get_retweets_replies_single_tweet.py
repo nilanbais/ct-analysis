680177abf81b83e4d360544d91f43bbdaad00cdf
Bais <32563996+nilanbais@users.noreply.github.com>
2022-04-29 18:02:36 +0200

    refactor

--- src/get_retweets_replies_single_tweet.py
+++
@@ -1,9 +0,0 @@
-"""
-This script is for the extraction of the replies and retweets off a single tweet. This script has to
-be executed multiple times to extract the replies and retweets of a set of tweets. The id of a tweet is
-obtained by the get_tweets_single_user.py.
-
-[] - Be able to extract the replies to a single tweet.
-[] - Be able to extract the retweets of a single tweet.
-[] - Be able to import the script or functions in other scripts.
-"""
