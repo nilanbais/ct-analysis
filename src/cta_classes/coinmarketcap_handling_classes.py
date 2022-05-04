a96a123e3d5623b460bd52fca916abde9cf448c7
Bais <32563996+nilanbais@users.noreply.github.com>
2022-05-04 22:19:18 +0200

    Update coinmarketcap_handling_classes.py

--- src/cta_classes/coinmarketcap_handling_classes.py
+++ src/cta_classes/coinmarketcap_handling_classes.py
@@ -9,8 +9,8 @@ import json
 import dotenv
 from pprint import pprint
 
-from base_classes.api_authentication_class import ApiAuthentication
-from base_classes.data_transformer_class import DataTransformer
+from cta_classes.base_classes.api_authentication_class import ApiAuthentication
+from cta_classes.base_classes.data_transformer_class import DataTransformer
 
 class CoinMarketCapAPI(ApiAuthentication):
 
@@ -74,15 +74,16 @@ class CoinMarketCapAPI(ApiAuthentication):
         json_response = self.connect_to_endpoint(authentication='api key', url=self.url, header=self.header, params=self.query_parameters)
         return json_response['data']
 
-    def extract_symbol_id_list(self, specified_symbols: list = None) -> dict:
+    def get_symbol_id_list(self, specified_symbols: list = []) -> dict:
         """Returns a list of coin ids. 
             If a list given, it returns the is of the specified coins.
             If no list given, the method will do a standard request.
         """
+
+        prepped_parameter_dict = {"symbol": self.data_trandform.list_to_string(input_list=specified_symbols)} if len(specified_symbols) > 0 else {}
+
         self.create_header()
-        self.create_query_parameters(
-            parameter_dict={"symbol": self.data_trandform.list_to_string(input_list=specified_symbols)}
-        )  # specified symbols toeveogen
+        self.create_query_parameters(parameter_dict=prepped_parameter_dict)  # specified symbols toeveogen
         self.create_url(mode='idmap')
 
         json_response = self.connect_to_endpoint(authentication='api key', url=self.url, header=self.header, params=self.query_parameters)
@@ -101,7 +102,7 @@ class CMCDataTransformer:
 
 def main() -> None:
     cmc_api = CoinMarketCapAPI()
-    data = cmc_api.extract_symbol_id_list(specified_symbols=['BTC', 'QNT'])
+    data = cmc_api.get_symbol_id_list(specified_symbols=['BTC', 'QNT'])
     pprint(data)
 
 if __name__ == '__main__':
