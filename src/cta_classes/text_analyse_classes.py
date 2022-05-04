30fc4435ae5b28742246022e5eb0e1d22065ff46
Bais <32563996+nilanbais@users.noreply.github.com>
2022-05-04 20:51:50 +0200

    Update text_analyse_classes.py

--- src/cta_classes/text_analyse_classes.py
+++ src/cta_classes/text_analyse_classes.py
@@ -1,7 +1,10 @@
 """
 Classes focussing on text analysis.
 """
+import re
 import textblob
+
+from typing import Union, List
 
 
 class CryptoSymbolFinder:
@@ -21,11 +24,20 @@ class CryptoSymbolFinder:
     def __init__(self) -> None:
         pass
 
-    def find_crypto_symbol(self, input_text: str) -> str:
+    def find_crypto_symbol(self, input_text: str) -> Union[List[str], str]:
         """
         Method used to find a crypto symbol and returns the symbols found
         """
-        pass
+        regex_string = r'\$[A-Z]{3}|[A-Z]{3}'
+        return re.search(regex_string, input_text)
+        """
+        Bovenstaande kan waasschijnlijk recursief. Neem daarbij de startindex van de tekst als veranderende waarde in de recursive call. 
+        break_statement is wanneer de start en eind index zo dicht bij elkaar liggen dat het gegarandeerd geen crypto symbool is.
+
+        Waarschijnlijk is het qua prestatie beter om eerst recursief een lijst met mogelijke crypto symbolen te verzamelen, en vervolgens deze te filteren
+        op de daadwerkelijke symbolen en de ruis die mee kwam door de regex string (QNT en het woord 'ALL' geven beide een hit).
+
+        """
 
 
 class SentimentAnalysis:
@@ -55,3 +67,23 @@ class TextAnalysis:
     def __init__(self) -> None:
         self.sentiment = SentimentAnalysis()
         self.symbol_finder = CryptoSymbolFinder()
+
+def main():
+    pass
+
+def test():
+    test_text = """
+                So to prevent QNT people from trading against him further, he reserves all the margin.
+
+                ALL OF IT
+
+                You couldn't borrow any LTC on spot anywhere. 
+                Since futures prices are marked against an index of spot prices, it became tough to move prices BTC enough to hit his liquidation.
+                """
+    csf = CryptoSymbolFinder()
+    print(csf.find_crypto_symbol(input_text=test_text))
+
+
+if __name__ == '__main__':
+    # main()
+    test()
