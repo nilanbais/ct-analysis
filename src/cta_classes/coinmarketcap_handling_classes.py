c8038ccc2b1681a5a1e8c9e829e3b399f876aeef
Bais <32563996+nilanbais@users.noreply.github.com>
2022-05-04 22:27:44 +0200

    Update coinmarketcap_handling_classes.py

--- src/cta_classes/coinmarketcap_handling_classes.py
+++ src/cta_classes/coinmarketcap_handling_classes.py
@@ -41,26 +41,15 @@ class CoinMarketCapAPI(ApiAuthentication):
     """
     Methods to manage reading and writing the data
     """
-    def read_json(self, file_name: str, folder_name: str) -> dict:
+    def _read_json(self, file_name: str, folder_name: str) -> dict:
         """
         Base method (a method that will be extrended to specific use cases)
         """
         with open("./{}/{}".format(folder_name, file_name), 'r') as json_file:
             return json.load(json_file)
 
-    def store_json(self, json_data: dict, file_name: str, folder_name: str) -> None:
-        """
-        THIS METHOD IS GOING TO BE REPLACED WITH THE COMMUNICATION WITH THE MONGODB DATABASE.
-        """
-        with open('./{}/{}'.format(folder_name, file_name), 'w') as json_file:
-            json.dump(json_data, json_file)
-
-
     def read_resource(self, file_name: str) -> dict:
-        return self.read_json(file_name=file_name, folder_name='res')
-
-    def store_response(self, json_data: dict, file_name: str) -> None:
-        return self.store_json(json_data=json_data, file_name=file_name, folder_name='data')
+        return self._read_json(file_name=file_name, folder_name='res')
 
     """
     Methods to get a response from the API
