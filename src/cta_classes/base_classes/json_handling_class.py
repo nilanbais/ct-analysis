19a3a40c4716a209ea3f557eb9838b7fb36f8ea8
Bais <32563996+nilanbais@users.noreply.github.com>
2022-05-05 15:59:08 +0200

    Update json_handling_class.py

--- src/cta_classes/base_classes/json_handling_class.py
+++ src/cta_classes/base_classes/json_handling_class.py
@@ -2,6 +2,7 @@
 class to be inhereted by classes to gain standard read/write json methods
 """
 import json
+import bson
 
 
 class JsonHandler:
@@ -9,9 +10,25 @@ class JsonHandler:
     def __init__(self) -> None:
         pass
 
-    def _read_json(self, file_name: str, folder_name: str) -> dict:
+    """ JSON file mehtods
+    """
+    def read_json(self, file_name: str, folder_name: str) -> dict:
         """
         Base method (a method that will be extrended to specific use cases)
         """
         with open("./{}/{}".format(folder_name, file_name), 'r') as json_file:
             return json.load(json_file)
+    
+    def store_json(self, json_data: dict, file_name: str, folder_name: str) -> None:
+        with open('./{}/{}'.format(folder_name, file_name), 'w') as json_file:
+            json.dump(json_data, json_file)
+        print('Done my dude')
+    
+    """ BSON file mehtods
+    """
+    def store_bson(self, bson_data: dict, file_name: str, folder_name: str) -> None:
+        with open("./{}/{}".format(folder_name, file_name), 'wb') as bson_file:
+            bson_file.write(bson_data)
+    
+    def encode_as_bson(self, input_data):
+        return bson.BSON.encode(input_data)
