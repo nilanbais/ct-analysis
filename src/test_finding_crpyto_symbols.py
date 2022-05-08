a4a16d3744f644d6f66cabd6e0b71836e05c3443
Bais <32563996+nilanbais@users.noreply.github.com>
2022-05-08 15:14:10 +0200

    Create test_finding_crpyto_symbols.py

---
+++ src/test_finding_crpyto_symbols.py
@@ -0,0 +1,11 @@
+"""
+Script voor het testen van de functionaliteit van het vinden van crypto symbols in tekst.
+
+De reden dat dit niet in hetzelfde document als de classes gebeurt, is omdat anders de import statements steeds aangepast
+moeten worden. Daar heb ik geen zin in.
+"""
+from cta_classes.text_analyse_classes import *
+
+
+if __name__ == '__main__':
+    test()
