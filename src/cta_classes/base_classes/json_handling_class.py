49cf9ead09a8baec91f1519f6f0b81994ac9cc96
Bais <32563996+nilanbais@users.noreply.github.com>
2022-05-08 15:14:00 +0200

    Update json_handling_class.py

--- src/cta_classes/base_classes/json_handling_class.py
+++ src/cta_classes/base_classes/json_handling_class.py
@@ -12,20 +12,20 @@ class JsonHandler:
 
     """ JSON file mehtods
     """
-    def read_json(self, file_name: str, folder_name: str) -> dict:
+    def _read_json(self, file_name: str, folder_name: str) -> dict:
         """
         Base method (a method that will be extrended to specific use cases)
         """
         with open("./{}/{}".format(folder_name, file_name), 'r') as json_file:
             return json.load(json_file)
     
-    def store_json(self, json_data: dict, file_name: str, folder_name: str) -> None:
+    def _store_json(self, json_data: dict, file_name: str, folder_name: str) -> None:
         with open('./{}/{}'.format(folder_name, file_name), 'w') as json_file:
             json.dump(json_data, json_file)
         print('Done my dude')
     
     """ BSON file mehtods
     """
-    def store_bson(self, bson_data: bytes, file_name: str, folder_name: str) -> None:
+    def _store_bson(self, bson_data: bytes, file_name: str, folder_name: str) -> None:
         with open("./{}/{}".format(folder_name, file_name), 'wb') as bson_file:
             bson_file.write(bson_data)
