814139faf360d7ca7b66fe5eeeb5a72d95050089
Bais <32563996+nilanbais@users.noreply.github.com>
2022-05-09 13:28:04 +0200

    Update database_handling_classes.py

--- src/cta_classes/database_handling_classes.py
+++ src/cta_classes/database_handling_classes.py
@@ -172,11 +172,11 @@ class DataBaseActions:
         """Private method to empty a given collection from the cta_databse.
         """
         user_input = input(f"{'-'*20}WARING{'-'*20}\nYou're about the empty the collection {collection_name}.\nDo you want to proceed? y/n\n")
-        if user_input == 'y':
+        if user_input in ['y', 'yes']:
             self.delete_many_documents(collection_name=collection_name, 
                                        query={})
             print("Done my dude")
-        elif user_input == 'n':
+        elif user_input in ['n', 'no']:
             print("Nothing happend. No worries.")
         else:
             print("I don't understand what you mean so I'm aborting the mission.")
