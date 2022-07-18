7d1b85a095faad37700a9c7d265b237d2511135f
Bais <32563996+nilanbais@users.noreply.github.com>
2022-07-18 18:29:21 +0200

    Create get_user_tweets.py

---
+++ src/get_user_tweets.py
@@ -0,0 +1,20 @@
+"""
+Script that builds a pipeline to extract the tweets of a given users for a timeperiod.
+"""
+
+
+
+# Get a user_id
+
+
+# define a time range (default 1 day??)
+
+
+# extract the tweets
+
+
+# clean the result
+
+
+# store the result
+
